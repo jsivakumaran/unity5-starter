e5e3383032c99535f08244a58468d8faebcddb3b
jsivakumaran <jsivakumaran@outlook.com>
2016-04-30 05:59:39 -0700

    Add collection of star with collisionbody

---
+++ Assets/PlayerCollect.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollect : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col){
+		//hit the star object
+		print(name + " has been collected");
+	}
+}
--- Assets/SimpleCharacter.cs
+++ Assets/SimpleCharacter.cs
@@ -3,10 +3,10 @@ using System.Collections;
 
 public class SimpleCharacter : MonoBehaviour {
 	//variables
+	public CharacterController MyController;
 	public float speed = 3f;
 	public float gravityStrength = 5f;  //gravity constant of earth is 9.8 - you may remember it from high school physics
 	public float jumpSpeed = 10f;
-	public CharacterController MyController;
 	public Transform CameraTransform;
 
 
@@ -17,7 +17,7 @@ public class SimpleCharacter : MonoBehaviour {
 		//get input from player
 		myVector.x = Input.GetAxis("Horizontal");
 		myVector.z = Input.GetAxis("Vertical");
-		myVector = myVector*speed*Time.deltaTime;  //Make it move with a particular speed
+		myVector = myVector * speed * Time.deltaTime;  //Make it move with a particular speed
 		//use input to move the character around
 		myVector = CameraTransform.rotation * myVector; //rotate input to direction of camera
 		float verticalVelocity =MyController.velocity.y - gravityStrength*Time.deltaTime;
