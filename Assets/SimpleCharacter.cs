2bcfc1297ae389c6d32d7f89f40844ace3fc6e13
jsivakumaran <jsivakumaran@outlook.com>
2016-04-30 05:39:40 -0700

    Jump added

--- Assets/SimpleCharacter.cs
+++ Assets/SimpleCharacter.cs
@@ -2,8 +2,14 @@
 using System.Collections;
 
 public class SimpleCharacter : MonoBehaviour {
+	//variables
 	public float speed = 3f;
+	public float gravityStrength = 5f;  //gravity constant of earth is 9.8 - you may remember it from high school physics
+	public float jumpSpeed = 10f;
 	public CharacterController MyController;
+	public Transform CameraTransform;
+
+
 	// Update is called once per frame
 	void Update () {
 
@@ -11,9 +17,18 @@ public class SimpleCharacter : MonoBehaviour {
 		//get input from player
 		myVector.x = Input.GetAxis("Horizontal");
 		myVector.z = Input.GetAxis("Vertical");
-		myVector *= speed;
+		myVector = myVector*speed*Time.deltaTime;  //Make it move with a particular speed
 		//use input to move the character around
+		myVector = CameraTransform.rotation * myVector; //rotate input to direction of camera
+		float verticalVelocity =MyController.velocity.y - gravityStrength*Time.deltaTime;
+		//How do we get the jump key?
+		if(Input.GetButtonDown("Jump")){
+			//add jumpspeed to vertical velocity
 
-		MyController.SimpleMove(myVector);
+			verticalVelocity += jumpSpeed;
+		}
+
+		myVector.y = verticalVelocity*Time.deltaTime; //add new speed to old speed to mimick acceleration
+		MyController.Move(myVector); //can use SimpleMove or Move(this is more complex)
 	}
 }
