87963a2bac4a6d7692694ec1750daf79df1c3d88
jsivakumaran <jsivakumaran@outlook.com>
2016-04-29 22:46:23 -0700

    Get user input to move the pill around

--- Assets/MyFirstScript.cs
+++ Assets/MyFirstScript.cs
@@ -2,10 +2,11 @@
 using System.Collections;
 
 public class MyFirstScript : MonoBehaviour {
-
+	public string whatToPrint;
+	public int numberToPrint =10;
 	// Use this for initialization
 	void Start () {
-		print("Hello World");
+		print(whatToPrint + " " + numberToPrint);
 	}
 	
 	// Update is called once per frame
---
+++ Assets/SimpleCharacter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleCharacter : MonoBehaviour {
+	public float speed = 3f;
+	public CharacterController MyController;
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 myVector = new Vector3(0, 0, 0);
+		//get input from player
+		myVector.x = Input.GetAxis("Horizontal");
+		myVector *= speed;
+		//use input to move the character around
+
+		MyController.SimpleMove(myVector);
+	}
+}
