79404024a5496ed66bf2d3f0c6b3857bd06abc50
jsivakumaran <jsivakumaran@outlook.com>
2016-04-30 04:39:27 -0700

    Capsule moves left and right, create platform, large plane and star as goal

--- Assets/SimpleCharacter.cs
+++ Assets/SimpleCharacter.cs
@@ -10,6 +10,7 @@ public class SimpleCharacter : MonoBehaviour {
 		Vector3 myVector = new Vector3(0, 0, 0);
 		//get input from player
 		myVector.x = Input.GetAxis("Horizontal");
+		myVector.z = Input.GetAxis("Vertical");
 		myVector *= speed;
 		//use input to move the character around
 
